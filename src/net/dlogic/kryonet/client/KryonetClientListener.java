b86f5bac87851109f0ae515bd194369ce02a82d3
macalimlim <macalimlim@gmail.com>
2013-09-10 10:04:58 +0800

    added and if-else block for incoming Responses

--- src/net/dlogic/kryonet/client/KryonetClientListener.java
+++ src/net/dlogic/kryonet/client/KryonetClientListener.java
@@ -1,9 +1,22 @@
 package net.dlogic.kryonet.client;
 
+import net.dlogic.kryonet.client.event.callback.IRoomEventCallback;
+import net.dlogic.kryonet.common.response.JoinRoomFailureResponse;
+import net.dlogic.kryonet.common.response.JoinRoomSuccessResponse;
+import net.dlogic.kryonet.common.response.LoginFailureResponse;
+import net.dlogic.kryonet.common.response.LoginSuccessResponse;
+import net.dlogic.kryonet.common.response.LogoutResponse;
+import net.dlogic.kryonet.common.response.PrivateMessageResponse;
+import net.dlogic.kryonet.common.response.PublicMessageResponse;
+
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 public class KryonetClientListener extends Listener {
+	private IRoomEventCallback roomEventCallback;
+	public void setRoomEventCallback(IRoomEventCallback callback) {
+		roomEventCallback = callback;
+	} 
 	public void connected(Connection connection) {
 		// TODO Auto-generated method stub
 		super.connected(connection);
@@ -13,11 +26,25 @@ public class KryonetClientListener extends Listener {
 		super.disconnected(connection);
 	}
 	public void received(Connection connection, Object object) {
-		// TODO Auto-generated method stub
-		super.received(connection, object);
+		if (object instanceof JoinRoomFailureResponse) {
+			JoinRoomFailureResponse response = (JoinRoomFailureResponse)object;
+			roomEventCallback.onJoinRoomFailure(response.errorMessage);
+		} else if (object instanceof JoinRoomSuccessResponse) {
+			JoinRoomSuccessResponse response = (JoinRoomSuccessResponse)object;
+			roomEventCallback.onJoinRoomSuccess();
+		} else if (object instanceof LoginFailureResponse) {
+			
+		} else if (object instanceof LoginSuccessResponse) {
+			
+		} else if (object instanceof LogoutResponse) {
+			
+		} else if (object instanceof PrivateMessageResponse) {
+			
+		} else if (object instanceof PublicMessageResponse) {
+			
+		}
 	}
 	public void idle(Connection connection) {
-		// TODO Auto-generated method stub
 		super.idle(connection);
 	}
 }
