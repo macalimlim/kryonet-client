4d8287bf781b42c04fa358387bc4601f296cb25d
macalimlim <macalimlim@gmail.com>
2013-09-11 18:01:46 +0800

    added methods addListener(), registerClass(), getKryonetClientListener(). refactored the class by separating the starting of the client to another method

--- src/net/dlogic/kryonet/client/KryonetClient.java
+++ src/net/dlogic/kryonet/client/KryonetClient.java
@@ -2,19 +2,39 @@ package net.dlogic.kryonet.client;
 
 import java.io.IOException;
 
+import net.dlogic.kryonet.client.event.callback.ILoginOrLogoutEventCallback;
+import net.dlogic.kryonet.client.event.callback.IPersonMessageEventCallback;
+import net.dlogic.kryonet.client.event.callback.IRoomEventCallback;
 import net.dlogic.kryonet.common.request.LoginRequest;
 import net.dlogic.kryonet.common.utility.KryonetUtility;
 
 import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Listener;
 
 public class KryonetClient {
-	private static Client client;
-	public KryonetClient(int writeBufferSize, int objectBufferSize, int timeout, String host, int tcpPort, int udpPort) throws IOException {
+	private Client client;
+	private KryonetClientListener listener;
+	public KryonetClient(int writeBufferSize, int objectBufferSize) {
 		client = new Client(writeBufferSize, objectBufferSize);
 		KryonetUtility.registerClasses(client);
-		client.start();
-		client.connect(timeout, host, tcpPort, udpPort);
-		client.addListener(new KryonetClientListener());
+		client.addListener(listener = new KryonetClientListener());
+	}
+	public void start(int timeout, String host, int tcpPort, int udpPort) throws IOException, KryonetClientException {
+		if (!client.isConnected()) {
+			client.start();
+			client.connect(timeout, host, tcpPort, udpPort);
+		} else {
+			throw new KryonetClientException("client already connected");
+		}
+	}
+	public KryonetClientListener getKryonetClientListener() {
+		return listener;
+	}
+	public void addListener(Listener listener) {
+		client.addListener(listener);
+	}
+	public void registerClass(Class type) {
+		client.getKryo().register(type);
 	}
 	public void sendLoginRequest(String username, String password) {
 		LoginRequest request = new LoginRequest();
