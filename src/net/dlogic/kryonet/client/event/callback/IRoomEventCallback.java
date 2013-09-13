a73492bd9bef3e00ac2d72ae9ed0dd653a3d1c3e
macalimlim <macalimlim@gmail.com>
2013-09-13 14:20:25 +0800

    added JoinedUSer and JoinedRoom parameters to the callback method

--- src/net/dlogic/kryonet/client/event/callback/IRoomEventCallback.java
+++ src/net/dlogic/kryonet/client/event/callback/IRoomEventCallback.java
@@ -1,6 +1,9 @@
 package net.dlogic.kryonet.client.event.callback;
 
+import net.dlogic.kryonet.common.entity.Room;
+import net.dlogic.kryonet.common.entity.User;
+
 public interface IRoomEventCallback {
 	public void onJoinRoomFailure(String errorMessage);
-	public void onJoinRoomSuccess();
+	public void onJoinRoomSuccess(User joinedUser, Room joinedRoom);
 }
