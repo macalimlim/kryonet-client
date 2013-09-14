576d2c724e679a4050bfafd977f17893d6d35e72
macalimlim <macalimlim@gmail.com>
2013-09-15 07:15:25 +0800

    made the Client and KryonetClientListener objects public for easy/fast access

--- src/net/dlogic/kryonet/client/KryonetClient.java
+++ src/net/dlogic/kryonet/client/KryonetClient.java
@@ -13,29 +13,27 @@ import net.dlogic.kryonet.common.request.PublicMessageRequest;
 import net.dlogic.kryonet.common.utility.KryonetUtility;
 
 import com.esotericsoftware.kryonet.Client;
-import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.minlog.Log;
 
 public class KryonetClient {
-	private Client client;
-	private KryonetClientListener listener;
+	public Client endpoint;
+	public KryonetClientListener listener;
 	public KryonetClient(int writeBufferSize, int objectBufferSize) {
 		Log.info("KryonetClient(" + writeBufferSize + ", " + objectBufferSize + ")");
-		client = new Client(writeBufferSize, objectBufferSize);
-		KryonetUtility.registerClasses(client);
-		client.addListener(listener = new KryonetClientListener());
+		endpoint = new Client(writeBufferSize, objectBufferSize);
+		KryonetUtility.registerClasses(endpoint);
+		endpoint.addListener(listener = new KryonetClientListener());
 	}
 	public void start(final int timeout, String host, int tcpPort, int udpPort) throws IOException, KryonetClientException {
 		Log.info("KryonetClient.start(" + timeout + ", " + host + ", " + tcpPort + ", " + udpPort + ")");
-		if (!client.isConnected()) {
-			client.start();
-			client.connect(timeout, host, tcpPort, udpPort);
+		if (!endpoint.isConnected()) {
+			endpoint.start();
+			endpoint.connect(timeout, host, tcpPort, udpPort);
 			new Thread(new Runnable() {
 				public void run() {
 					try {
-						client.update(timeout);
+						endpoint.update(timeout);
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
@@ -44,47 +42,38 @@ public class KryonetClient {
 			throw new KryonetClientException("client already started");
 		}
 	}
-	public KryonetClientListener getKryonetClientListener() {
-		return listener;
-	}
-	public void addListener(Listener listener) {
-		client.addListener(listener);
-	}
-	public void registerClass(Class type) {
-		client.getKryo().register(type);
-	}
 	public void sendJoinRoomRequest(Room roomToJoin, String password) {
 		JoinRoomRequest request = new JoinRoomRequest();
 		request.targetRoomId = roomToJoin.id;
 		request.password = password;
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 	public void sendLeaveRoomRequest(Room roomToLeave) {
 		LeaveRoomRequest request = new LeaveRoomRequest();
 		request.targetRoomId = roomToLeave.id;
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 	public void sendLoginRequest(String username, String password) {
 		Log.info("KryonetClient.sendLoginRequest(" + username + ", " + password + ")");
 		LoginRequest request = new LoginRequest();
 		request.username = username;
 		request.password = password;
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 	public void sendLogoutRequest() {
 		LogoutRequest request = new LogoutRequest();
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 	public void sendPrivateMessageRequest(User targetUser, String message) {
 		PrivateMessageRequest request = new PrivateMessageRequest();
 		request.targetUserId = targetUser.id;
 		request.message = message;
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 	public void sendPublicMessageRequest(Room targetRoom, String message) {
 		PublicMessageRequest request = new PublicMessageRequest();
 		request.targetRoomId = targetRoom.id;
 		request.message = message;
-		client.sendTCP(request);
+		endpoint.sendTCP(request);
 	}
 }
