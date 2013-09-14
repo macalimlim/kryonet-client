edc7ad76d8f28f0d41c78d2d8297059d2b8a4a20
macalimlim <macalimlim@gmail.com>
2013-09-15 02:44:26 +0800

    added ConnectionEventCallback for running/testing

---
+++ app/ConnectionEventCallback.java
@@ -0,0 +1,12 @@
+import net.dlogic.kryonet.client.event.callback.IConnectionEventCallback;
+
+
+public class ConnectionEventCallback implements IConnectionEventCallback {
+
+	@Override
+	public void onDisconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
