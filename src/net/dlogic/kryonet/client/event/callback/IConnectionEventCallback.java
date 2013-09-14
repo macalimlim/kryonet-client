c3cfaadce29a86b2396ed3478073677fe41f7521
macalimlim <macalimlim@gmail.com>
2013-09-15 07:09:31 +0800

    added inConnected method to be implemented in its subclasses

--- src/net/dlogic/kryonet/client/event/callback/IConnectionEventCallback.java
+++ src/net/dlogic/kryonet/client/event/callback/IConnectionEventCallback.java
@@ -1,5 +1,6 @@
 package net.dlogic.kryonet.client.event.callback;
 
 public interface IConnectionEventCallback {
-	public void onDisconnect();
+	public void onConnected();
+	public void onDisconnected();
 }
