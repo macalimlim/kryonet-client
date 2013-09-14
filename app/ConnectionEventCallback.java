ddcb8989c93ae85fad61686ce6ee4b85f4d40715
macalimlim <macalimlim@gmail.com>
2013-09-15 07:16:43 +0800

    implemented the onConnected method from the interface

--- app/ConnectionEventCallback.java
+++ app/ConnectionEventCallback.java
@@ -4,9 +4,15 @@ import net.dlogic.kryonet.client.event.callback.IConnectionEventCallback;
 public class ConnectionEventCallback implements IConnectionEventCallback {
 
 	@Override
-	public void onDisconnect() {
+	public void onDisconnected() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void onConnected() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
