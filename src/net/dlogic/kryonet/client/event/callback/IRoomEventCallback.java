1b1b69b4528cf62f1acbecfda87aef554124a1c9
macalimlim <macalimlim@gmail.com>
2013-09-15 09:19:24 +0800

    added onGetRooms method

--- src/net/dlogic/kryonet/client/event/callback/IRoomEventCallback.java
+++ src/net/dlogic/kryonet/client/event/callback/IRoomEventCallback.java
@@ -1,9 +1,12 @@
 package net.dlogic.kryonet.client.event.callback;
 
+import java.util.List;
+
 import net.dlogic.kryonet.common.entity.Room;
 import net.dlogic.kryonet.common.entity.User;
 
 public interface IRoomEventCallback {
+	public void onGetRooms(List<Room> roomList);
 	public void onJoinRoomFailure(String errorMessage);
 	public void onJoinRoomSuccess(User joinedUser, Room joinedRoom);
 }
