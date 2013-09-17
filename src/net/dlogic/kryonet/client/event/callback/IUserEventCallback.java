3c7e4416581dd8272a6371dca7526f22c01beccd
macalimlim <macalimlim@gmail.com>
2013-09-17 09:24:57 +0800

    replaced ILoginOrLogoutEventCallback with IUserEventCallback

--- src/net/dlogic/kryonet/client/event/callback/IUserEventCallback.java
+++ src/net/dlogic/kryonet/client/event/callback/IUserEventCallback.java
@@ -2,7 +2,7 @@ package net.dlogic.kryonet.client.event.callback;
 
 import net.dlogic.kryonet.common.entity.User;
 
-public interface ILoginOrLogoutEventCallback {
+public interface IUserEventCallback {
 	public void onLoginFailure(String errorMessage);
 	public void onLoginSuccess(User myself);
 	public void onLogout();
