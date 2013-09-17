baa4b494bb358ee5ec776152ea609c89a7754dcb
macalimlim <macalimlim@gmail.com>
2013-09-17 14:34:20 +0800

    sets Myself object upon new connection, unsets Myself object upon disconnection, on recieving the JoinRoomSuccessResponse, added code to put the joinedUser in a room and add a user to the user manager

--- src/net/dlogic/kryonet/client/KryonetClientListener.java
+++ src/net/dlogic/kryonet/client/KryonetClientListener.java
@@ -2,9 +2,14 @@ package net.dlogic.kryonet.client;
 
 import net.dlogic.kryonet.client.event.callback.IConnectionEventCallback;
 import net.dlogic.kryonet.client.event.callback.IErrorEventCallback;
-import net.dlogic.kryonet.client.event.callback.IUserEventCallback;
 import net.dlogic.kryonet.client.event.callback.IPersonMessageEventCallback;
 import net.dlogic.kryonet.client.event.callback.IRoomEventCallback;
+import net.dlogic.kryonet.client.event.callback.IUserEventCallback;
+import net.dlogic.kryonet.common.entity.Myself;
+import net.dlogic.kryonet.common.manager.MyselfInstance;
+import net.dlogic.kryonet.common.manager.RoomManager;
+import net.dlogic.kryonet.common.manager.RoomManagerException;
+import net.dlogic.kryonet.common.manager.UserManager;
 import net.dlogic.kryonet.common.response.ErrorResponse;
 import net.dlogic.kryonet.common.response.GetRoomsResponse;
 import net.dlogic.kryonet.common.response.JoinRoomFailureResponse;
@@ -20,6 +25,7 @@ import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.minlog.Log;
 
 public class KryonetClientListener extends Listener {
+	private Myself myself = MyselfInstance.myself;
 	private IErrorEventCallback errorEventCallback;
 	private IConnectionEventCallback connectionEventCallback;
 	private IRoomEventCallback roomEventCallback;
@@ -44,9 +50,19 @@ public class KryonetClientListener extends Listener {
 		personMessageEventCallback = callback;
 	}
 	public void connected(Connection connection) {
+		myself.id = connection.getID();
+		myself.isAdmin = false;
+		myself.isItMe = true;
+		myself.isPlayer = false;
+		myself.roomManager.map.clear();
 		connectionEventCallback.onConnected();
 	}
 	public void disconnected(Connection connection) {
+		myself.username = null;
+		myself.isAdmin = false;
+		myself.isItMe = false;
+		myself.isPlayer = false;
+		myself.roomManager.map.clear();
 		connectionEventCallback.onDisconnected();
 	}
 	public void received(Connection connection, Object object) {
@@ -61,15 +77,36 @@ public class KryonetClientListener extends Listener {
 			JoinRoomFailureResponse response = (JoinRoomFailureResponse)object;
 			roomEventCallback.onJoinRoomFailure(response.errorMessage);
 		} else if (object instanceof JoinRoomSuccessResponse) {
-			JoinRoomSuccessResponse response = (JoinRoomSuccessResponse)object;
-			roomEventCallback.onJoinRoomSuccess(response.joinedUser, response.joinedRoom);
+			try {
+				JoinRoomSuccessResponse response = (JoinRoomSuccessResponse)object;
+				response.joinedUser.isItMe = (myself.id == response.joinedUser.id) ? true : false;
+				UserManager userManager = myself.userManager;
+				RoomManager roomManager = myself.roomManager;
+				if (!roomManager.map.containsKey(response.joinedRoom.name)) {
+					roomManager.map.put(response.joinedRoom.name, response.joinedRoom);
+				}
+				userManager.map.put(response.joinedUser.id, response.joinedUser);
+				roomManager.addUserToRoom(response.joinedUser, response.joinedRoom.name);
+				roomEventCallback.onJoinRoomSuccess(response.joinedUser, response.joinedRoom);
+			} catch (RoomManagerException e) {
+				roomEventCallback.onJoinRoomFailure(e.getMessage());
+			}
 		} else if (object instanceof LoginFailureResponse) {
 			LoginFailureResponse response = (LoginFailureResponse)object;
 			loginOrLogoutEventCallback.onLoginFailure(response.errorMessage);
 		} else if (object instanceof LoginSuccessResponse) {
 			LoginSuccessResponse response = (LoginSuccessResponse)object;
+			myself.username = response.myself.username;
+			myself.isAdmin = response.myself.isAdmin;
+			myself.isItMe = response.myself.isItMe;
+			myself.isPlayer = response.myself.isPlayer;
 			loginOrLogoutEventCallback.onLoginSuccess(response.myself);
 		} else if (object instanceof LogoutResponse) {
+			myself.username = null;
+			myself.isAdmin = false;
+			myself.isItMe = false;
+			myself.isPlayer = false;
+			myself.roomManager.map.clear();
 			loginOrLogoutEventCallback.onLogout();
 		} else if (object instanceof PrivateMessageResponse) {
 			PrivateMessageResponse response = (PrivateMessageResponse)object;
